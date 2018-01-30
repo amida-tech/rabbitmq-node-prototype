f8f29d43f8dde726ed3ea19110dee5f831d1c4a0
Jacob Sachs <jacob.s.sachs@gmail.com>
2018-01-30 14:02:59 -0500

    token exchange working

--- server.js
+++ server.js
@@ -1,38 +1,43 @@
-#!/usr/bin/env node
+/** Configuration Info
+ * q: name of the queue (service)
+ * host: full host address of the queue (service)
+ */
+var q = 'auth_queue';
+var host = 'amqp://localhost'
+var secret = 'bad_secret';
 
 var amqp = require('amqplib');
+var jwt  = require('jsonwebtoken');
 
-function fib(n) {
-  // Do it the ridiculous, but not most ridiculous, way. For better,
-  // see http://nayuki.eigenstate.org/page/fast-fibonacci-algorithms
-  var a = 0, b = 1;
-  for (var i=0; i < n; i++) {
-    var c = a + b;
-    a = b; b = c;
-  }
-  return a;
+function token() {
+  var userInfo = {
+    id: 'dummy-id',
+    username: 'dummy-username',
+    email: 'dummy@email.com'
+  };
+
+  return jwt.sign(userInfo, secret);
 }
 
-amqp.connect('amqp://localhost').then(function(conn) {
-  process.once('SIGINT', function() { conn.close(); });
-  return conn.createChannel().then(function(ch) {
-    var q = 'rpc_queue';
-    var ok = ch.assertQueue(q, {durable: false});
-    var ok = ok.then(function() {
+amqp.connect(host).then(function (conn) {
+  process.once('SIGINT', function () { conn.close(); });
+  return conn.createChannel().then(function (ch) {
+    var ok = ch.assertQueue(q, { durable: false });
+    var ok = ok.then(function () {
       ch.prefetch(1);
       return ch.consume(q, reply);
     });
-    return ok.then(function() {
+    return ok.then(function () {
       console.log(' [x] Awaiting RPC requests');
     });
 
     function reply(msg) {
       var n = parseInt(msg.content.toString());
-      console.log(' [.] fib(%d)', n);
-      var response = fib(n);
+      console.log(' [.] token request');
+      var response = token();
       ch.sendToQueue(msg.properties.replyTo,
-                     Buffer.from(response.toString()),
-                     {correlationId: msg.properties.correlationId});
+        Buffer.from(response.toString()),
+        { correlationId: msg.properties.correlationId });
       ch.ack(msg);
     }
   });
